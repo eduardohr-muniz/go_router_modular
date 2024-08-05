626d8ae6e3a3807f7f56a8c40b23256e1d38e9d7
eduardohr-muniz <eduardohr.muniz@outlook.com>
2024-08-05 11:13:19 -0300

    :bookmark: version: 1.0.4 ajuste exports

--- lib/go_router_modular.dart
+++ lib/go_router_modular.dart
@@ -1,13 +1,15 @@
 library go_router_modular;
 
 export 'src/bind.dart';
-export 'src/routes/child_route.dart';
-export 'src/routes/module_route.dart';
-export 'src/i_module.dart';
-export 'src/route_manager.dart';
 export 'src/go_router_modular_configure.dart';
-export 'src/routes/i_modular_route.dart';
+export 'src/i_module.dart';
+export 'src/injector.dart';
 export 'src/page_transition_enum.dart';
-export 'src/routes/shell_modular_route.dart';
+export 'src/route_manager.dart';
 export 'src/route_model.dart';
+export 'src/transition.dart';
+export 'src/routes/child_route.dart';
+export 'src/routes/i_modular_route.dart';
+export 'src/routes/module_route.dart';
+export 'src/routes/shell_modular_route.dart';
 export 'package:go_router/go_router.dart' hide GoRouter;
